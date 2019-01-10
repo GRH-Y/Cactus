7b555d49660b4b63f9c1be3f7f075ad5dd54e29c
ydz <584335070@qq.com>
2019-01-10 13:33:49 +0800

    升级jar包

--- app/src/main/java/com/yyz/ard/cactus/network/ArdConvertResult.java
+++ app/src/main/java/com/yyz/ard/cactus/network/ArdConvertResult.java
@@ -5,22 +5,33 @@ import com.yyz.ard.cactus.xml.XmlParser;
 
 import connect.network.base.JavConvertResult;
 import json.JsonUtils;
+import util.GZipUtils;
 import util.LogDog;
 
 public class ArdConvertResult extends JavConvertResult {
 
     @Override
-    public Object handlerEntity(Class resultCls, byte[] result) {
+    public Object handlerEntity(Class resultCls, byte[] result, String encode) {
         if (resultCls == null || result == null) {
             return null;
         }
-        String jsonStr = new String(result);
-        LogDog.d("==> Request to return the content = " + jsonStr);
+
         Object entity;
-        if (jsonStr.startsWith("<?xml")) {
-            entity = XmlParser.parserToEntity(jsonStr, resultCls);
+        byte[] newData = result;
+        if ("gzip".equals(encode)) {
+            byte[] unCompressData = GZipUtils.unCompress(result);
+            newData = unCompressData == null ? result : unCompressData;
+        }
+        if (resultCls.isAssignableFrom(byte[].class)) {
+            return newData;
+        }
+
+        String resultStr = new String(newData);
+        LogDog.d("==> Request to return the content = " + resultStr);
+        if (resultStr.startsWith("<?xml")) {
+            entity = XmlParser.parserToEntity(resultStr, resultCls);
         } else {
-            entity = JsonUtils.toEntity(resultCls, jsonStr);
+            entity = JsonUtils.toEntity(resultCls, resultStr);
         }
         return entity;
     }
